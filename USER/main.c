eb24acb158b191940275333b6c9982ec41d8da50
baba <ya89621@gmail.com>
2022-12-09 11:38:20 +0800

    test
    Signed-off-by: baba <ya89621@gmail.com>

--- USER/main.c
+++ USER/main.c
@@ -87,10 +87,8 @@ s32 main(void)
 		I2C_WR_EepromInit();
     while(1) 
 			{
-				LED1_TOGGLE();//Make sure function worked
         LED2_TOGGLE();//Make sure function worked
 				LED3_TOGGLE();//Make sure function worked
-				LED4_TOGGLE();//Make sure function worked
 				i2c_data_read(I2C1,0x00,i2c_rxbuffer,4);//This ffunction is to read I2C Slave data
 				printf(" %x H: %x M: %x S day %x\n",i2c_rxbuffer[2],i2c_rxbuffer[1],i2c_rxbuffer[0],i2c_rxbuffer[3]);//Use debug port to see data
 				DELAY_Ms(998);//delay one Sec to Read data(or use Timer
